b754b3ade76383f0a14f6f01410da9653e62be47
Hector Chu <hectorchu@gmail.com>
2026-05-18 17:25:17 +0100

    run mweb in a separate thread

--- bindings/modmweb_bindings.c
+++ bindings/modmweb_bindings.c
@@ -6,24 +6,28 @@ void tinygo_init(void *heap, size_t heap_size, void *glob, void *glob_end, void 
 char *mweb(const char *fn, const char *req);
 extern char _data_start, _bss_end;
 
+static mp_obj_t mp_mweb_init(mp_obj_t stack_size_obj, mp_obj_t heap_size_obj) {
+    int stack_size = mp_obj_get_int(stack_size_obj);
+    int heap_size = mp_obj_get_int(heap_size_obj);
+
+    void *stack = pxTaskGetStackStart(NULL) + stack_size;
+    tinygo_init(malloc(heap_size), heap_size, &_data_start, &_bss_end, stack);
+
+    return mp_const_none;
+}
+static MP_DEFINE_CONST_FUN_OBJ_2(init_obj, mp_mweb_init);
+
 static mp_obj_t mp_mweb(mp_obj_t fn_obj, mp_obj_t req_obj) {
-    static bool init;
-    if (!init) {
-        int heap = 500000;
-        void *stack = pxTaskGetStackStart(NULL) + MICROPY_TASK_STACK_SIZE;
-        tinygo_init(malloc(heap), heap, &_data_start, &_bss_end, stack);
-        init = true;
-    }
     char *resp = mweb(mp_obj_str_get_str(fn_obj), mp_obj_str_get_str(req_obj));
     mp_obj_t resp_obj = mp_obj_new_str(resp, strlen(resp));
     free(resp);
     return resp_obj;
 }
-
 static MP_DEFINE_CONST_FUN_OBJ_2(mweb_obj, mp_mweb);
 
 static const mp_rom_map_elem_t mweb_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_mweb_go) },
+    { MP_ROM_QSTR(MP_QSTR_init), MP_ROM_PTR(&init_obj) },
     { MP_ROM_QSTR(MP_QSTR_mweb), MP_ROM_PTR(&mweb_obj) },
 };
 static MP_DEFINE_CONST_DICT(mweb_module_globals, mweb_module_globals_table);
