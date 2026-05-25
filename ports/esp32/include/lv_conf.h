e1b87953fc4d5fef58055815f8fdc5c037a7a72f
Hector Chu <hectorchu@gmail.com>
2026-05-25 10:54:15 +0100

    solve remaining memory issues

--- ports/esp32/include/lv_conf.h
+++ ports/esp32/include/lv_conf.h
@@ -1,10 +1,11 @@
 #ifndef LV_CONF_H
 #define LV_CONF_H
 
-#define LV_MEM_POOL_ALLOC malloc
-#define LV_MEM_SIZE 2800000
-#define LV_USE_LODEPNG 1
-#define LV_USE_OS LV_OS_FREERTOS
-#define LV_USE_PRIVATE_API 1
+#define LV_MEM_POOL_ALLOC                   malloc
+#define LV_MEM_SIZE                         (3072 * 1024)
+#define LV_USE_FREERTOS_TASK_NOTIFY         1
+#define LV_USE_LODEPNG                      1
+#define LV_USE_OS                           LV_OS_FREERTOS
+#define LV_USE_PRIVATE_API                  1
 
 #endif
--- ports/esp32/include/mpconfig_esp32.h
+++ ports/esp32/include/mpconfig_esp32.h
@@ -1,7 +1,13 @@
+#define MICROPY_GC_INITIAL_HEAP_SIZE        (3584 * 1024)
+#define MP_PLAT_ALLOC_HEAP                  malloc
+
 #include <mpconfigport.h>
 
 #undef MICROPY_FLOAT_IMPL
 #define MICROPY_FLOAT_IMPL                  (MICROPY_FLOAT_IMPL_DOUBLE)
+
+#undef MICROPY_GC_SPLIT_HEAP_AUTO
+#define MICROPY_GC_SPLIT_HEAP_AUTO          (0)
 
 #undef MICROPY_PY_MACHINE_ADC
 #undef MICROPY_PY_MACHINE_ADC_BLOCK
